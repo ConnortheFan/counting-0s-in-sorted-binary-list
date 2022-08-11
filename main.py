50c06706be2c582951c003eac844c4fe704b782c
Connor Fan <110417453+ConnortheFan@users.noreply.github.com>
2022-08-11 16:26:19 +0000

    Included starting comments

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+# A “binary list” is a list containing only 0s and 1s. For example, [0, 1, 0, 0, 1] is a binary list. 
+# A “sorted binary list” (in ascending order for this problem) is a binary list such that no 1s are ever followed by 0s. 
+# For example, [0, 0, 0, 1, 1] is a sorted binary list. 
+# Given a sorted binary list containing n elements, we can determine the number of 0s as follows:
+
+# 	Algorithm count_0s(nums):
+# 		For i from 0 to |nums|-1:  # using 0-based indexing
+# 			If nums[i] is 1:
+# 				Return i + 1
+# 		Return |nums|
+
+# However, for a list containing n elements, this simple algorithm requires us to look at all n elements in the worst-case scenario (all elements are 0s), which can become slow as n becomes large. 
+# Can we compute the number of 0s in a sorted binary list by looking at fewer than n elements?
+
+
 def count_0s(list):
   midpoint = len(list)//2
   zeros = 0
